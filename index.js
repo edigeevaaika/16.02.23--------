3f5672fcacb51db87b00f8c651700a24f0d472ec
Aida <edigeeva.aika@mail.ru>
2023-02-16 15:28:21 +0600

    used a loop in an array

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ console.log(example1[7]);
 
 
 console.log(example2[1][1]);
-
+4,15,1,2,77
 let test = example2[2];
 console.log(test[1]);
 
@@ -20,7 +20,7 @@ for( let i = 0; i < example1.length; i ++){
 
  for( let i = 0; i < example2.length; i ++){
   for( let j = 0; j < example2.length; j ++){
-  console.log (example2[i][j]); 
+  // console.log (example2[i][j]); 
 }
 }
 
@@ -31,3 +31,26 @@ console.log("yes");
 else {
   console.log("no");
 }
+
+let example4 = [5,4,1,55,1,0,2];
+let example5 = [3,11,4,15,1,2,77];
+
+for( let i = 0; i < example4.length; i ++){
+  if (example5.includes(example4[i]))
+  console.log (example4[i]); 
+}
+
+// let arrResult = []
+// function arrSort() {
+//   example4.forEach(value => {
+//     if(example5.includes(value) && !arrResult.includes(value)) {
+//       arrResult.push(value)
+//     }
+//   })
+//   return arrResult
+// }
+// let result = arrSort();
+// console.log(result);
+
+let example6 = ["a","b","c","f","d",];
+console.log(example6.reverse());
